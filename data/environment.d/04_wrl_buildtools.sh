0dbe4c15a2ad52c19bf9687c9045d0e00775cd7e
Mark Hatle <mark.hatle@windriver.com>
2016-12-02 16:54:41 +0000

    04_wrl_buildtools.sh: update to buildtools 20161202
    Signed-off-by: Mark Hatle <mark.hatle@windriver.com>

--- data/environment.d/04_wrl_buildtools.sh
+++ data/environment.d/04_wrl_buildtools.sh
@@ -16,7 +16,7 @@
 # Download, install and load the buildtools tarball (as needed)
 
 # Buildtools location can change -- this is the path on top of the BASEURL
-BUILDTOOLS_REMOTE="${BUILDTOOLS_REMOTE:-buildtools-standalone-20161122}"
+BUILDTOOLS_REMOTE="${BUILDTOOLS_REMOTE:-buildtools-standalone-20161202}"
 
 # Special windshare folders to search
 BUILDTOOLS_FOLDERS="WRLinux-9-LTS-CVE WRLinux-9-LTS WRLinux-9-Base"
