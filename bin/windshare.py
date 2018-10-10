c08a4a8ee7a0695f1efc17e5f9554e835b7a7bf7
Mark Hatle <mark.hatle@windriver.com>
2018-10-10 15:42:10 -0400

    windshare: Adjust to point to lts.18 control file
    Signed-off-by: Mark Hatle <mark.hatle@windriver.com>

--- bin/windshare.py
+++ bin/windshare.py
@@ -64,7 +64,7 @@ class Windshare():
             return (None, None, None)
 
         # Magic URL to the entitlement file
-        ws_entitlement_url = ws_base_url + '/wrlinux-lts.17.json'
+        ws_entitlement_url = ws_base_url + '/wrlinux-lts.18.json'
 
         logger.debug('Entitlement url %s' % ws_entitlement_url)
 
