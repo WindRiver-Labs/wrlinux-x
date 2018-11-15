cda5f8df9f7fa84260edc3fb5b62311dd198bae2
Mark Hatle <mark.hatle@windriver.com>
2018-11-15 17:46:06 -0500

    04_wrl_buildtools.sh: Move to buildtools-tarball-20181115
    Signed-off-by: Mark Hatle <mark.hatle@windriver.com>

--- data/environment.d/04_wrl_buildtools.sh
+++ data/environment.d/04_wrl_buildtools.sh
@@ -16,7 +16,7 @@
 # Download, install and load the buildtools tarball (as needed)
 
 # Buildtools location can change -- this is the path on top of the BASEURL
-BUILDTOOLS_REMOTE="${BUILDTOOLS_REMOTE:-buildtools-standalone-20181005}"
+BUILDTOOLS_REMOTE="${BUILDTOOLS_REMOTE:-buildtools-standalone-20181115}"
 
 # Special windshare folders to search
 BUILDTOOLS_FOLDERS="WRLinux-lts-18-Core WRLinux-lts-18-Base"
