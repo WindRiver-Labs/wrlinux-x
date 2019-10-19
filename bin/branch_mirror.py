WindRiver-Labs/wrlinux-x

commit f0bfa2c7a0058f3fcf09c17bdc3765edfd75c56e

setup: Support checking out by tag instead of branch
Issue: LINUXEXEC-1839

Some operations need to be split into a fetch and checkout in order to work
with tags instead of branches.

Define a rule to determine how to convert a branch into a tag name.  Since
the branch of a index will have the branch and not tag name.  The rules are:

A tag must start with 'vWRLINUX_'.  If it contains _RCPL or _UPDATE it is a
product tag.  Otherwise it's considered to be a CI tag.  And then must end in
a version, starting with '10.'.

For instance product tag, vWRLINUX_10_18_UPDATE0007, refers to product branch
WRLINUX_10_18_UPDATE0007.

While a CI tag, vWRLINUX_CI_10.19.35.0, refers to a CI branch
WRLINUX_CI.

Once the conversion is known, we adjust the layerindex branch information to
refer to the tag instead of branch.  (Tags always are prefixed by
'refs/tags/vWRLINUX' in order to be identified.)

Additionally the setup.sh will not only detect the tag, but will also detect
a checkout that is related to tag, but not actually on the tag and force the
user to select a specific branch or tag.  (This should only ever happen on
internal development.)  If this is detected, the user will get a message:

Checkout may be on a detached HEAD, this HEAD does not appear to
correspond to a specific, tag.  (It appears you may be working with
tag <tag>.  If this is correct, use
--base-branch=<tag> in the arguments to setup.sh.

Signed-off-by: Mark Hatle <mark.hatle@windriver.com>

bin/branch_mirror.py
@@ -84,7 +84,11 @@ work_list = []
 def git_branch(_dst, _orig_branch, _branch):
     logger.info('Branching %s: %s -> %s' % (_dst, _orig_branch, _branch))
 
-    _cmd = [ 'git', 'fetch', '.', '%s:%s' % (_orig_branch, _branch) ]
+    # Break this into two commands, so it will work with tags as well...
+    _cmd = [ 'git', 'fetch', '.', '%s' % (_orig_branch) ]
+    utils_setup.run_cmd(_cmd, cwd=_dst)
+
+    _cmd = [ 'git', 'branch', '%s', 'FETCH_HEAD' % (_branch) ]
     if force:
         _cmd.append('-f')
     utils_setup.run_cmd(_cmd, cwd=_dst)

bin/setup.py
@@ -269,7 +269,11 @@ class Setup():
             utils_setup.run_cmd(cmd, log=2, environment=self.env, cwd=mirror_index)
 
         try:
-            cmd = [self.tools['git'], 'fetch', '-f', '-n', '-u', remote_mirror, self.base_branch + ':' + folder + self.base_branch]
+            # We don't know if we're fetching a branch or tag, if it's a tag we have to do this
+            # in two steps anyway, so always go to 'FETCH_HEAD' and then branch it.
+            cmd = [self.tools['git'], 'fetch', '-n', '-u', remote_mirror, self.base_branch]
+            utils_setup.run_cmd(cmd, log=2, environment=self.env, cwd=mirror_index)
+            cmd = [self.tools['git'], 'checkout', '-B', folder + self.base_branch, 'FETCH_HEAD']
             utils_setup.run_cmd(cmd, log=2, environment=self.env, cwd=mirror_index)
         except:
             # Could not fetch, return
@@ -363,6 +367,25 @@ class Setup():
                   ]
 
         self.index = Layer_Index(indexcfg=settings.INDEXES, base_branch=self.base_branch, replace=replace, mirror=mirror_index_path)
+
+        # Is this a Wind River tag? if so... we need to modify the 'branches' entries to be the same as the tag
+        if self.base_branch.startswith('refs/tags/vWRLINUX'):
+            if '_RCPL' in self.base_branch or '_UPDATE' in self.base_branch:
+                # vWRLINUX_<something>_UPDATExxxx
+                # vWRLINUX_<something>_RCPLxxxx
+                new_base_branch = self.base_branch[11:]
+            else:
+                # vWRLINUX_<something>_10.19.25.0
+                new_base_branch_pos = self.base_branch.find('10.')
+                new_base_branch = self.base_branch[:(new_base_branch_pos-1)][11:]
+
+            logger.debug('Detected a WR tag, replacing %s with %s...' % (self.base_branch[11:], new_base_branch))
+            for lindex in self.index.index:
+                for branch in lindex['branches']:
+                    if branch and branch['bitbake_branch'] == new_base_branch:
+                        branch['bitbake_branch'] = self.base_branch
+                    if branch and branch['name'] == new_base_branch:
+                        branch['name'] = self.base_branch
 
     def process_layers(self):
         from collections import deque

setup.sh
@@ -190,7 +190,14 @@ fi
 if [ -z "${BASEBRANCH}" ]; then
 	BASEBRANCH=$(git --git-dir="$BASEDIR/.git" rev-parse --abbrev-ref HEAD)
 	if [ "$BASEBRANCH" = "HEAD" ]; then
-		BASEBRANCH=""
+		# Maybe this is a tag instead?
+		BASEBRANCH=$(git --git-dir="$BASEDIR/.git" describe HEAD 2>/dev/null)
+		if [ $? -ne 0 ]; then
+			# No reasonable branch/tag name found...
+			BASEBRANCH=""
+		else
+			BASEBRANCH="refs/tags/$BASEBRANCH"
+                fi
 	fi
 fi
 
@@ -225,9 +232,21 @@ if [ $help -ne 1 ]; then
 	esac
 
 	if [ -z "$BASEBRANCH" ]; then
-		echo "May be on a detached HEAD, HEAD must be on a branch. ($BASEDIR)" >&2
+		echo "May be on a detached HEAD, HEAD must be on a branch or tag. ($BASEDIR)" >&2
 		echo "You can avoid this by passing the branch using --base-branch=" >&2
 		exit 1
+	fi
+
+        # Is this a tag?  If so, don't allow tags w/ '-'
+	if [ "$BASEBRANCH" != "${BASEBRANCH##refs/tags/}" ]; then
+		if [ "$BASEBRANCH" != "${BASEBRANCH//-*}" ]; then
+			echo "Checkout may be on a detached HEAD, this HEAD does not appear to" >&2
+			echo "correspond to a specific, tag.  (It appears you may be working with" >&2
+			echo "tag ${BASEBRANCH//-*}.  If this is correct, use" >&2
+			echo "--base-branch=${BASEBRANCH//-*} in the arguments to" >&2
+			echo "$0."
+			exit 1
+		fi
 	fi
 
 	for func in "${ADDFUNCS[@]}"; do

commit b5a4dc9f0d3eac3dd8b64d1dd1ad9e9460b788b6

branch_mirror.py: Fix creation or moving of RCPL branch
Fixes a typo introduced in f0bfa2c7a0058f3fcf09c17bdc3765edfd75c56e

Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

bin/branch_mirror.py
@@ -88,7 +88,7 @@ def git_branch(_dst, _orig_branch, _branch):
     _cmd = [ 'git', 'fetch', '.', '%s' % (_orig_branch) ]
     utils_setup.run_cmd(_cmd, cwd=_dst)
 
-    _cmd = [ 'git', 'branch', '%s', 'FETCH_HEAD' % (_branch) ]
+    _cmd = [ 'git', 'branch', _branch, 'FETCH_HEAD' ]
     if force:
         _cmd.append('-f')
     utils_setup.run_cmd(_cmd, cwd=_dst)