ea7841db1938ac036ad4b2d85e57c92de9684f57
Konrad Scherer <Konrad.Scherer@windriver.com>
2020-01-07 16:20:51 -0500

    The json file on Wind Share is named after the release number which for CD release is 'cd'
    Issue: LINCD-800
    
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- bin/windshare.py
+++ bin/windshare.py
@@ -64,7 +64,7 @@ class Windshare():
             return (None, None, None)
 
         # Magic URL to the entitlement file
-        ws_entitlement_url = ws_base_url + '/wrlinux-ci.json'
+        ws_entitlement_url = ws_base_url + '/wrlinux-cd.json'
 
         logger.debug('Entitlement url %s' % ws_entitlement_url)
 
