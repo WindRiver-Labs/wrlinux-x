63b20cf4ce85b75592e521818fc58efc528d57aa
Mingli Yu <mingli.yu@windriver.com>
2020-11-26 06:59:14 +0000

    01_wrl_buildtools.sh: enable buildtools
    Issue: LINUXEXEC-10887
    
    Before LIN1019, 01_wrl_buildtools.sh exists, but 01_wrl_buildtools.sh
    is missing when enable buildtools in below commit since LIN1019.
    79e0c42876 data/environment.d: Enable buildtools
    
    Add it back to make sure anspass database works as 02_wrl_anspass.sh will
    call anspass_setup to start anspass database(anspassd) in a project which
    has run setup before, but there is no anspassd if 01_wrl_buildtools.sh is
    missing.
    
    Signed-off-by: Mingli Yu <mingli.yu@windriver.com>

---
+++ data/environment.d/01_wrl_buildtools.sh
@@ -0,0 +1,37 @@
+# Copyright (C) 2016, 2020 Wind River Systems, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+# Check if the buildtools are available, if so load it early...
+
+BUILDTOOLS="${BUILDTOOLS:-bin/buildtools}"
+
+setup_add_func buildtools_pre_setup
+
+unset -v BUILDTOOLS_LOADED
+
+buildtools_pre_setup() {
+	ENVIRON=$(find -L ${BUILDTOOLS} -name "environment-setup-${SDKARCH}-*-linux" 2>/dev/null | head -n1)
+	if [ -z "${ENVIRON}" ]; then
+		# Nothing there yet...
+		return 0
+	fi
+	. "${ENVIRON}"
+	if [ $? -ne 0 ]; then
+		# Something went wrong.. ignore it
+		return 0
+	fi
+	BUILDTOOLS_LOADED=1
+	return 0
+}
